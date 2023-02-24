047094e875694f037764f6a08e0259446664b193
danielwzr <danielandrade@unipam.edu.br>
2023-02-24 10:22:04 -0300

    js aula

---
+++ index.js
@@ -0,0 +1,25 @@
+var botaoBorda = document.getElementById("botao-borda");
+var conteudo = document.getElementById("conteudo");
+var textoBorda = document.getElementById("texto-borda");
+
+var clicked = false;
+var numeroDeCliques = 0;
+
+botaoBorda.addEventListener("click", ()=> {
+    if (clicked == false){
+        console.log(numeroDeCliques)
+        numeroDeCliques++;
+        conteudo.style.outline = "5px solid orange";
+        clicked = true;     
+        botaoBorda.innerText = "Remover borda";
+        textoBorda.innerText = "Você clicou " + numeroDeCliques + "x";
+        textoBorda.style.transition = "0.3s ease";
+        textoBorda.style.transform = `rotate(${numeroDeCliques}deg)`;
+    }
+    else {
+        conteudo.style.outline = "0px solid orange";
+        clicked = false;
+        botaoBorda.innerText = "Adicionar borda";
+    }
+});
+
